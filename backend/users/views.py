139a16f7892be197d8780fad44dc77cc2379192b
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-09-21 21:15:14 +0300

    ADDED: requirements, REFACTORED: RegisterAPIView post method

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,15 +11,17 @@ class RegisterAPIView(APIView):
 
     def post(self, request):
         serializer = UserAuthSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        refresh = RefreshToken.for_user(user)
-        return Response(
-            {
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-                "user_id": user.id,
-                "username": user.username,
-            },
-            status=status.HTTP_201_CREATED,
-        )
+
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    "user_id": user.id,
+                    "username": user.username,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
